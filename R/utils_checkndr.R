72660c9d8e657e120e545019d1a48d738f2d09c7
Stephen Balogun <77954949+stephenbalogun@users.noreply.github.com>
2023-03-24 01:12:22 +0100

    create download option names for the two possible line lists.

--- R/utils_checkndr.R
+++ R/utils_checkndr.R
@@ -6,16 +6,25 @@
 #'
 #' @examples NULL
 #'
+
 recency_download_opts <- function() {
+  c(opts_one(), opts_two())
+}
+
+
+opts_one <-  function() {
+  c("Client state", "Client LGA", "Sex", "Age", "Visit date", "Screening result", "Confirmatory test",
+    "Tie breaker", "Testing point")
+}
+
+opts_two <- function() {
   c(
-    "Client state", "Client LGA", "Sex", "Age", "Screening result", "Confirmatory test", "Tie breaker",
-    "Testing point", "Opt out", "Recency test name", "Recency test date", "Recency test before visit date",
+    "Opt out", "Recency test name", "Recency test date", "Recency test before visit date",
     "Recency number", "Invalid recency number", "Control line", "Verification line", "Longterm line",
     "Interpreted longterm", "Interpreted recent", "Interpreted negative", "Interpreted invalid",
     "Viral load requested", "VL sample collection date", "Viral load result", "Partial duplicates"
   )
 }
-
 
 
 #' Recency Line-list Variables
